2db509d4d4355aea35751b4436fd4e0f4d50675b
lisasce <lisa.66@hotmail.com>
2021-12-13 15:25:16 +0100

    reinstall boostrap 5 + create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  public isLoggedIn = false;
+  public myTitle= '';
+  public pokeball = 'https://cdn.pixabay.com/photo/2016/08/15/00/50/pokeball-1594373_960_720.png';
+  public pokeballOpened = 'https://cdn.pixabay.com/photo/2019/11/18/15/46/pokemon-4635112_960_720.png';
+
+  collapsed = true;
+  constructor(private router: Router, private titleService: Title) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle("My Pokemon Library");
+    this.myTitle = this.titleService.getTitle();
+    this.isLoggedIn = true;
+  }
+
+  goHome(): void {
+    // this.router.navigate(['/pokemons/all']);
+  }
+
+  logout(): void  {
+    //this.authService.logout();
+    //this.router.navigate(['/login']);
+  }
+
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+}
