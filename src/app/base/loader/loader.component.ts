f93173cdcb35999e260c4126c3705be0f2061384
lisasce <lisa.66@hotmail.com>
2021-12-15 14:04:02 +0100

    implement bootstrap loader

--- src/app/base/loader/loader.component.ts
+++ src/app/base/loader/loader.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbProgressbarConfig} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-loader',
-  templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+  template: `
+    <p><ngb-progressbar [value]="500" type="teal"></ngb-progressbar></p>
+  `,
+  providers: [NgbProgressbarConfig]
 })
 export class LoaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbProgressbarConfig) {
+    // customize default values of progress bars used by this component tree
+    config.max = 1000;
+    config.striped = true;
+    config.animated = true;
+    config.type = 'success';
+    config.height = '20px';
+  }
 
   ngOnInit(): void {
   }
