9f6f6fb232279187234f07da23460cccd6fb7488
lisasce <lisa.66@hotmail.com>
2021-12-16 14:08:08 +0100

    adapt template driven login form

--- src/app/base/authentification/login/login.component.ts
+++ src/app/base/authentification/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../auth.service";
 import {Router} from "@angular/router";
+import {UserModel} from "../user-model";
 
 @Component({
   selector: 'app-login',
@@ -11,8 +12,12 @@ export class LoginComponent implements OnInit {
   authServiceCheckLogin= false;
 
   public message: string = 'You are not logged in. (pokeball/pokeball)';
-  public name = '';
-  public  password = '';
+
+  public user: UserModel = {
+    name: '',
+    password: ''
+  }
+
   public connectingStatus = false;
 
   constructor(private authService: AuthService, private router: Router) { }
@@ -24,7 +29,8 @@ export class LoginComponent implements OnInit {
     //
   }
 
-  login() {
-    //
+  login(user : UserModel) {
+    this.connectingStatus = true;
+    console.log(this.user)
   }
 }
