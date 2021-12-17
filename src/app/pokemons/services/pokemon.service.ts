3e9cf59bd0cb48965e4c734faf8f112d2ac8b5d8
lisasce <lisa.66@hotmail.com>
2021-12-17 15:09:50 +0100

    reformat service

--- src/app/pokemons/services/pokemon.service.ts
+++ src/app/pokemons/services/pokemon.service.ts
@@ -1,39 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {PokemonTypes} from "../pokemonStucture/pokemon-types";
 import {PokemonModel} from "../pokemonStucture/PokemonModel";
 import {HttpClient} from "@angular/common/http";
 import {catchError, Observable, of, tap} from "rxjs";
 
-
-
+// peut recevoir d'autres dependences
 @Injectable({
   providedIn: 'root'
 })
-
-@Injectable() // peut recevoir d'autres dependences
 export class PokemonService {
 
   private pokemonsUrl = 'api/pokemons';
 
-  private log(log: string) {
-    console.info(log);
+  constructor(private http: HttpClient) {
   }
 
-  private handleError<T>(operation= 'operation', result?: T){
-    return (error: any): Observable<T> => {
-      console.log(error);
-      console.log(`${operation} failed: ${error.message}`);
-      return of(result as T); //make the result observable
-    };
-  }
-
-  constructor(private http: HttpClient) {  }
-
-
-
-
-
-  newPokemon(
+  public newPokemon(
     id = null,
     hp = 45,
     cp = 25,
@@ -46,26 +28,16 @@ export class PokemonService {
     return pokemon;
   }
 
-
-  pokemonUrlGenerator():string{
-    const num = (Math.floor(Math.random() * 893) + 13).toString().padStart(3,'0');
-    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${num}.png`;
-  }
-
-  idGenerator():number{
-    return Math.floor(Math.random() * 893) + 13;
-  }
-
-  getPokemons(): Observable<PokemonModel[ ]> {
+  public getPokemons(): Observable<PokemonModel[ ]> {
     return this.http.get<PokemonModel[]>(this.pokemonsUrl).pipe(
-      tap(pokemons => this.log(`fetched pokemons: ${pokemons}`) ),
+      tap(pokemons => this.log(`fetched pokemons: ${pokemons}`)),
       catchError(this.handleError(`getPokemons`, []))
     );
   }
 
   // tap: Perform a side effect for every emission on the source Observable, but return an Observable that is identical to the source, Intercepts each emission on the source and runs a function, but returns an output which is identical to the source as long as errors don't occur. https://rxjs.dev/api/operators/tap
 
-  getPokemon(id: number): Observable<PokemonModel>{
+  public getPokemon(id: number): Observable<PokemonModel> {
     const url = `${this.pokemonsUrl}/${id}`;
 
     return this.http.get<PokemonModel>(url).pipe(
@@ -74,16 +46,36 @@ export class PokemonService {
     );
   }
 
-  getPokemonTypes(): Array<string>{
+  public getPokemonTypes(): Array<string> {
 
-    let pokemonTypes:Array<string>= [];
+    let pokemonTypes: Array<string> = [];
 
-    for(let value in PokemonTypes) {
-        pokemonTypes.push(value);
+    for (let value in PokemonTypes) {
+      pokemonTypes.push(value);
     }
     console.log(pokemonTypes);
     return pokemonTypes;
   }
 
+  private log(log: string) {
+    console.info(log);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T); //make the result observable
+    };
+  }
+
+  private pokemonUrlGenerator(): string {
+    const num = (Math.floor(Math.random() * 893) + 13).toString().padStart(3, '0');
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${num}.png`;
+  }
+
+  private idGenerator(): number {
+    return Math.floor(Math.random() * 893) + 13;
+  }
 
 }
