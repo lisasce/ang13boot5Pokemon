464ebdeb92d6c9e6f523f596ec949133a39be92a
lisasce <lisa.66@hotmail.com>
2021-12-22 11:49:47 +0100

    implement form component (wip)

--- src/app/pokemons/components/form-template/form-template.component.ts
+++ src/app/pokemons/components/form-template/form-template.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {PokemonModel} from "../../pokemonStucture/PokemonModel";
-import {FormBuilder, FormControl, FormGroup, Validator, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {PokemonService} from "../../services/pokemon.service";
 import {Router} from "@angular/router";
 import {PokemonTypes} from "../../pokemonStucture/pokemon-types";
@@ -16,28 +16,35 @@ export class FormTemplateComponent implements OnInit {
   @Input() pokemon?: PokemonModel;
   public PokemonTypes = PokemonTypes;
 
-  public pokemonForm: FormGroup;
   nameCtrl: FormControl;
   hpCtrl: FormControl;
   cpCtrl: FormControl;
+  //typesCtrl: FormControl;
+  public pokemonForm: FormGroup;
 
-  // typesCtrl: FormControl;
 
   constructor(formbuilder: FormBuilder, private pokemonsService: PokemonService, private router: Router) {
     // build form in constructor
 
-    this.nameCtrl = formbuilder.control('', [Validators.required, Validators.minLength(3)]);
-    this.hpCtrl = formbuilder.control(0, [Validators.required, Validators.max(999)]);
-    this.cpCtrl = formbuilder.control(0, [Validators.required, Validators.max(999)]);
+    this.nameCtrl = formbuilder.control('', [Validators.required, Validators.minLength(3),Validators.maxLength(25), Validators.pattern('^[a-zA-Z0-9àéèç]{1,25}$')]);
+    this.hpCtrl = formbuilder.control('', [Validators.required, Validators.max(999),Validators.pattern('^[0-9]{1,3}$')]);
+    this.cpCtrl = formbuilder.control('', [Validators.required, Validators.max(99),Validators.pattern('^[0-9]{1,2}$')]);
+
 
     this.pokemonForm = formbuilder.group({
       name: this.nameCtrl,
       hp: this.hpCtrl,
-      cp: this.cpCtrl
+      cp: this.cpCtrl,
+      types: formbuilder.array([], [Validators.required])
     })
   }
 
   ngOnInit(): void {
+  }
+
+  public isDefaultPokemonLoaded(): boolean{
+    const defaultPokemon = this.pokemonsService.newPokemon(new Date());
+    return this.pokemon?.picture === defaultPokemon.picture;
   }
 
   onSubmit(): void {
@@ -58,32 +65,32 @@ export class FormTemplateComponent implements OnInit {
     this.cpCtrl.setValue(0);
   }
 
-  hasType(type: string): boolean {
+  hasType(type: PokemonTypes): boolean {
     if (this.pokemon) {
       const pokemonHasType = this.pokemon.types.map(typesEnum => typesEnum.valueOf()).includes(type);
-      console.log(this.pokemon.name + ' has type ' + type + ' '
-      +pokemonHasType);
       return pokemonHasType;
     }
     return false;
   }
 
-  selectType($event: any, type: string): void {
-   /* if (this.pokemon) {
+  selectType($event: any, type: PokemonTypes): void {
+
+    if (this.pokemon) {
       const checked = $event.target.checked;
       if (checked) {
         this.pokemon.types.push(type);
+        console.log(this.pokemon.types)
       } else {
         const index = this.pokemon.types.indexOf(type);
         if (index > -1) {
           this.pokemon.types.splice(index, 1);
         }
       }
-    }*/
+    }
   }
 
-  isTypesValid(type: string): void {
-   /* if (this.pokemon){
+  isTypesValid(type: PokemonTypes): boolean {
+    if (this.pokemon){
       if (this.pokemon.types.length === 1 && this.hasType(type)) {
         return false;
       }
@@ -91,7 +98,7 @@ export class FormTemplateComponent implements OnInit {
         return false;
       }
     }
-    return true;*/
+    return true;
   }
 
 }
