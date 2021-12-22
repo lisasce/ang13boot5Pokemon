872e982c97372b3d90dbcefbe390b2b9e0ccc545
lisasce <lisa.66@hotmail.com>
2021-12-22 18:05:25 +0100

    implement form component (wip)

--- src/app/pokemons/components/form-template/form-template.component.ts
+++ src/app/pokemons/components/form-template/form-template.component.ts
@@ -33,7 +33,7 @@ export class FormTemplateComponent implements OnInit {
     this.nameCtrl = formbuilder.control('', [Validators.required, Validators.minLength(3), Validators.maxLength(25), Validators.pattern('^[a-zA-Z0-9àéèç]{1,25}$')]);
     this.hpCtrl = formbuilder.control('', [Validators.required, Validators.max(999), Validators.pattern('^[0-9]{1,3}$')]);
     this.cpCtrl = formbuilder.control('', [Validators.required, Validators.max(99), Validators.pattern('^[0-9]{1,2}$')]);
-    this.types = formbuilder.array([], [Validators.minLength(1),Validators.maxLength(3)]);
+    this.types = formbuilder.array([], []);
 
 
     this.pokemonForm = formbuilder.group({
@@ -56,23 +56,6 @@ export class FormTemplateComponent implements OnInit {
     return this.pokemon?.picture === defaultPokemon.picture;
   }
 
-  onSubmit(): void {
-    console.log(this.pokemonForm.value);
-    // this.pokemonService.submitPokemon(this.pokemon.subscribe(()=> this.goBack());
-  }
-
-  goBack(): void {
-    if (this.pokemon?.id) {
-      const link = ['/pokemons', this.pokemon.id];
-      this.router.navigate(link);
-    }
-  }
-
-  reset(): void {
-    this.nameCtrl.setValue('');
-    this.hpCtrl.setValue(0);
-    this.cpCtrl.setValue(0);
-  }
 
   hasType(type: PokemonTypes): boolean {
 
@@ -129,6 +112,25 @@ export class FormTemplateComponent implements OnInit {
       return false;
     }
     return true;
+  }
+
+
+  onSubmit(): void {
+    console.log(this.pokemonForm.value);
+    // this.pokemonService.submitPokemon(this.pokemon.subscribe(()=> this.goBack());
+  }
+
+  goBack(): void {
+    if (this.pokemon?.id) {
+      const link = ['/pokemons', this.pokemon.id];
+      this.router.navigate(link);
+    }
+  }
+
+  reset(): void {
+    this.nameCtrl.setValue('');
+    this.hpCtrl.setValue(0);
+    this.cpCtrl.setValue(0);
   }
 
 
